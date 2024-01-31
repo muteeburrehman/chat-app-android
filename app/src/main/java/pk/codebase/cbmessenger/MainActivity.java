196fa6f5013ccd2b1564d6915d81262da857f959
muteeburrehman <muteeb285@gmail.com>
2024-01-31 20:19:09 +0500

    firebase setup and login form ui setup

--- app/src/main/java/pk/codebase/cbmessenger/MainActivity.java
+++ app/src/main/java/pk/codebase/cbmessenger/MainActivity.java
@@ -2,13 +2,24 @@ package pk.codebase.cbmessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // if the user is not logged in then the user will directed to login page
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() == null){
+            Intent intent = new Intent(MainActivity.this,login.class);
+            startActivity(intent);
+        }
     }
 }
